23c1062d96d188f0bee5e887e17fceef28aff57f
Lawrence Ling <Lawrence1995@hotmail.com>
2015-09-28 05:27:29 +1000

    Reorganised Program.cs code

--- UtilityTool/Program.cs
+++ UtilityTool/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UtilityTool {
     static class Program {
+
+        // The form which is opened by clicking the tray icon
+        public static MainForm MainForm { get; private set; }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -13,7 +14,10 @@ namespace UtilityTool {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            MainForm = new MainForm();
+
+            Application.Run(MainForm);
         }
     }
 }
