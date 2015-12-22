b1369a33bc2ccb3908ca1508d96b8934d7c34a2a
Lawrence Ling <Lawrence1995@hotmail.com>
2015-12-22 14:41:26 +1100

    Renamed actions/events to tasks

---
+++ UtilityTool/Tasks/ProcessExecuteTask.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace UtilityTool.Tasks {
+    public class ProcessExecuteTask {
+
+        public ProcessExecuteTask(string path) {
+            Process.Start(@path);
+        }
+
+    }
+}
