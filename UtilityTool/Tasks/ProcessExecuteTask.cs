6cb9e9696383abb03c4c5766d8d96484fcc46ed2
Lawrence Ling <Lawrence1995@hotmail.com>
2016-01-03 05:29:03 +1100

    Added option to close MainForm on run for ProcessExecuteTask

--- UtilityTool/Tasks/ProcessExecuteTask.cs
+++ UtilityTool/Tasks/ProcessExecuteTask.cs
@@ -3,13 +3,21 @@
 namespace UtilityTool.Tasks {
     public class ProcessExecuteTask : ITask {
         private string Path;
+        private bool CloseOnRun;
 
-        public ProcessExecuteTask(string path) {
+        public ProcessExecuteTask(string path) : this(path, false) {
+        }
+
+        public ProcessExecuteTask(string path, bool closeOnRun) {
             Path = path;
+            CloseOnRun = closeOnRun;
         }
 
         public void Run() {
             Process.Start(Path);
+            if (CloseOnRun) {
+                Program.MainForm.Hide();
+            }
         }
     }
 }
