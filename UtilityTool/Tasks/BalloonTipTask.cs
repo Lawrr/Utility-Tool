a35506920a3e18650632c07cebd0a393d8965e71
Lawrence Ling <Lawrence1995@hotmail.com>
2016-01-06 09:15:10 +1100

    Added BalloonTipTask

---
+++ UtilityTool/Tasks/BalloonTipTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UtilityTool.Tasks {
+    public class BalloonTipTask : ITask {
+        public int TipTimeout { get; private set; }
+        public string TipTitle { get; private set; }
+        public string TipText { get; private set; }
+        public ToolTipIcon TipIcon { get; private set; }
+
+        public BalloonTipTask(long tipTimeout, string tipTitle, string tipText, string tipIcon)
+            : this((int)tipTimeout, tipTitle, tipText, tipIcon) {
+        }
+
+        public BalloonTipTask(int tipTimeout, string tipTitle, string tipText, string tipIcon) {
+            TipTimeout = tipTimeout;
+            TipTitle = tipTitle;
+            TipText = tipText;
+            try {
+                TipIcon = (ToolTipIcon)Enum.Parse(typeof(ToolTipIcon), tipIcon);
+            } catch (ArgumentException) {
+                TipIcon = ToolTipIcon.None;
+            }
+        }
+
+        public void Run() {
+            Program.Tray.TrayIcon.ShowBalloonTip(TipTimeout, TipTitle, TipText, TipIcon);
+        }
+    }
+}
